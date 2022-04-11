011ab3cf091395692543f5b3b06a83880f49d6dc
Patrik Trefil <36563492+PatrikTrefil@users.noreply.github.com>
2022-04-11 18:03:14 +0200

    exercise 3 - increaser, decreaser

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,57 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
+type CounterProp = {
+  counter: number;
+  setCounter: Function;
+};
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/increaser">Increaser</Link>
+        <Link to="/decreaser">Decreaser</Link>
       </nav>
       <hr />
       <Routes>
         <Route path="/" element={<TextGizmo />}></Route>
         <Route path="/about" element={<About />} />
+        <Route
+          path="/increaser"
+          element={<Increaser counter={counter} setCounter={setCounter} />}
+        />
+        <Route
+          path="/decreaser"
+          element={<Decreaser counter={counter} setCounter={setCounter} />}
+        />
       </Routes>
     </Router>
+  );
+}
+
+function Increaser(props: CounterProp) {
+  return (
+    <div>
+      <h2>Increaser</h2>
+      <p>Current count: {props.counter}</p>
+      <button onClick={() => props.setCounter(props.counter + 1)}>
+        Increase counter
+      </button>
+    </div>
+  );
+}
+function Decreaser(props: CounterProp) {
+  return (
+    <div>
+      <h2>Decreaser</h2>
+      <p>Current count: {props.counter}</p>
+      <button onClick={() => props.setCounter(props.counter - 1)}>
+        Decrease counter
+      </button>
+    </div>
   );
 }
 
